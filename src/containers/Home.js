0e686f23dac0c560746ebc263e5a50a209f5df90
maria.hernandez <maria.hernandez@loopup.com>
2017-10-22 20:15:05 -0700

    clean uo

--- src/containers/Home.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { getSiteProps } from 'react-static'
-//
-export default getSiteProps(() => <h1>Hello world {'<'}3</h1>)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 import React from 'react';
@@ -7,42 +6,9 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      Hello world!
+      Hello world! {'<3'}
     </div>
   );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-=======
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-
-// Your top level component
-import App from './App'
-
-// Export your top level component as JSX (for static rendering)
-export default <App />
-
-// Render your app
-if (typeof document !== 'undefined') {
-  const render = Comp => {
-    ReactDOM.hydrate(
-      <AppContainer>
-        <Comp />
-      </AppContainer>,
-      document.getElementById('root'),
-    )
-  }
-
-  // Render!
-  render(App)
-
-  // Hot Module Replacement
-  if (module.hot) {
-    module.hot.accept('./App', () => {
-      render(require('./App').default)
-    })
-  }
-}
->>>>>>> 580cad4589eb136291c643e975c41e18cd9038f8
