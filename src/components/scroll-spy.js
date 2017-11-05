f2c02c0c1425b637b7b588bb0cdda34eb4405fee
maria.hernandez <maria.hernandez@loopup.com>
2017-11-04 19:26:41 -0700

    simplify navbar scrolling

--- src/components/scroll-spy.js
+++ src/components/scroll-spy.js
@@ -12,21 +12,9 @@ class ScrollSpy extends Component {
     event.stopPropagation();
     const target = document.getElementById(event.target.getAttribute('data-scrollto'));
     if (target) {
-      let targetPosition = target.offsetTop || target.clientTop;
-      console.log('targetPosition: ', targetPosition);
-
-      if (targetPosition >= 0) {
-        targetPosition += (targetPosition !== 0) ? 5 : 0;
-        const init = window.scrollY;
-        const valueToAdd = (window.scrollY <= targetPosition) ? 10 : -10;
-        let i = window.scrollY;
-        const scrollInterval = setInterval(() => {
-          window.scrollTo(init, i);
-          i += valueToAdd;
-          if ((valueToAdd === 10 && i >= targetPosition) ||
-            (valueToAdd === -10 && i <= targetPosition)) clearInterval(scrollInterval);
-        }, 7);
-      }
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
     }
   }
 
