49be956fb1a76958351263738c57fd5237f4215a
elbecita <mariafehe@gmail.com>
2017-12-21 09:23:49 -0800

    about shortened

--- src/components/about.js
+++ src/components/about.js
@@ -19,14 +19,11 @@ const About = () => {
               expert on it, and here we are! in the path towards that, always learning, always having fun. Of course, I've worked with many other
               technologies. If you're curious, you'll be able to see more details about my background in my `}
               <a href="public/maria_fernandez_resume.pdf" target="_blank">resume</a></p>.
-            <p align="justify">On other aspects, I love reading (mostly fantasy and sci-fi) and travelling (ok, yes, typical), I used
-              to play videogames a lot when I was younger, but sadly free time when you're an adult is very little. I've been on & off during
-              my whole life trying to learn how to play guitar (and still there), and if I were rich I'd start a micro-brewery and study
-              History, Philosophy and Physics degrees during my free time (and of course I'd continue coding on my personal projects!).</p>
-            <p align="justify">Speaking about personal projects, the whole idea about this website was to start sharing with the world the
+            <hr />
+            <p align="justify">The whole idea about this website was to start sharing with the world the
               things I work on during my free time, so don't be shy, scroll down a bit and you'll find my projects there. The section
               will grow over time, I hope!</p>
-            <p align="justify">Also, if you want to contact me, just jump into any of my social profiles and send me a message. ^^</p>
+            <p align="justify">Also, if you want to contact me, just jump into any of my social profiles and send me a message. :)</p>
           </div>
         </div>
       </section>
